131d46484b9dc259fcdf332a9f656ba1cfcdcde2
yuan0520 <abcdabcd10211@gmail.com>
2024-07-13 16:49:33 +0800

    init

---
+++ docs/history_changelog.md
@@ -0,0 +1,16 @@
+# History of Changelog
+
+- **Aug 15, 2023**. Add `st-lpips` and `laion_aes`. Refer to official repo at [ShiftTolerant-LPIPS](https://github.com/abhijay9/ShiftTolerant-LPIPS) and [improved-aesthetic-predictor](https://github.com/christophschuhmann/improved-aesthetic-predictor)
+- **Aug 05, 2023**. Add our work [TOPIQ](https://arxiv.org/abs/2308.03060) with remarkable performance on almost all benchmarks via efficient Resnet50 backbone. Use it with `topiq_fr, topiq_nr, topiq_iaa` for Full-Reference, No-Reference and Aesthetic assessment respectively.
+- **March 30, 2023**. Add [URanker](https://github.com/RQ-Wu/UnderwaterRanker) for IQA of under water images. 
+- **March 29, 2023**. :rotating_light: Hot fix of NRQM & PI. 
+- **March 25, 2023**. Add TreS, HyperIQA, CNNIQA, CLIPIQA.
+- **Sep 1, 2022**. 1) Add pretrained models for MANIQA and AHIQ. 2) Add dataset interface for pieapp and PIPAL.
+- **June 3, 2022**. Add FID metric. See [clean-fid](https://github.com/GaParmar/clean-fid) for more details.
+- **March 11, 2022**. Add pretrained DBCNN, NIMA, and official model of PieAPP, paq2piq.
+- **March 5, 2022**. Add NRQM, PI, ILNIQE metrics.
+- **Feb 2, 2022**. Add MUSIQ inference code, and the converted official weights. See [Official codes](https://github.com/google-research/google-research/tree/master/musiq).
+
+## Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=chaofengc/IQA-PyTorch&type=Date)](https://star-history.com/#chaofengc/IQA-PyTorch&Date)
---
+++ my_inference.py
@@ -0,0 +1,90 @@
+import argparse
+import glob
+import os
+from pyiqa import create_metric
+from tqdm import tqdm
+import csv
+
+
+def main():
+    """Inference demo for pyiqa.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default=None, help='input image/folder path.')
+    parser.add_argument('-r', '--ref', type=str, default=None, help='reference image/folder path if needed.')
+    parser.add_argument(
+        '--metric_mode',
+        type=str,
+        default='FR',
+        help='metric mode Full Reference or No Reference. options: FR|NR.')
+    parser.add_argument('-m', '--metric_names', type=str, nargs='+', default=['PSNR'], help='IQA metric names, case sensitive.')
+    parser.add_argument('--save_file', type=str, default=None, help='path to save results.')
+
+    args = parser.parse_args()
+
+    # set up IQA models for each metric
+    iqa_models = {metric_name.lower(): create_metric(metric_name.lower(), metric_mode=args.metric_mode) for metric_name in args.metric_names}
+
+    if os.path.isfile(args.input):
+        input_paths = [args.input]
+        if args.ref is not None:
+            ref_paths = [args.ref]
+    else:
+        input_paths = sorted(glob.glob(os.path.join(args.input, '*')))
+        if args.ref is not None:
+            ref_paths = sorted(glob.glob(os.path.join(args.ref, '*')))
+
+    if args.save_file:
+        sf = open(args.save_file, 'w', newline='')
+        sfwriter = csv.writer(sf)
+
+    avg_scores = {metric_name: 0 for metric_name in args.metric_names}
+    test_img_num = len(input_paths)
+
+    pbar = tqdm(total=test_img_num, unit='image')
+    for idx, img_path in enumerate(input_paths):
+        img_name = os.path.basename(img_path)
+        if args.metric_mode == 'FR':
+            ref_img_path = ref_paths[idx]
+        else:
+            ref_img_path = None
+
+        row = [img_name]
+        for metric_name, iqa_model in iqa_models.items():
+            if metric_name != 'fid':
+                score = iqa_model(img_path, ref_img_path).cpu().item()
+                avg_scores[metric_name] += score
+                row.append(score)
+                pbar.set_description(f'{metric_name} of {img_name}: {score}')
+                pbar.write(f'{metric_name} of {img_name}: {score}')
+            else:
+                assert os.path.isdir(args.input), 'input path must be a folder for FID.'
+                score = iqa_model(args.input, args.ref)
+                avg_scores[metric_name] += score
+                row.append(score)
+                pbar.set_description(f'{metric_name} of {img_name}: {score}')
+                pbar.write(f'{metric_name} of {img_name}: {score}')
+
+        if args.save_file:
+            sfwriter.writerow(row)
+        pbar.update(1)
+
+    pbar.close()
+    for metric_name in avg_scores:
+        avg_scores[metric_name] /= test_img_num
+
+    for metric_name, avg_score in avg_scores.items():
+        msg = f'Average {metric_name} score of {args.input} with {test_img_num} images is: {avg_score}'
+        print(msg)
+        if args.save_file:
+            sf.write(f'{msg}\n')
+
+    if args.save_file:
+        sf.close()
+        print(f'Done! Results are in {args.save_file}.')
+    else:
+        print(f'Done!')
+
+
+if __name__ == '__main__':
+    main()
